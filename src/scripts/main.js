3cffea8b9d486c806f566ab0dbdaa918eafa2d86
Jhonatan David <82552187+jotahdavid@users.noreply.github.com>
2022-09-02 18:04:00 -0300

    :sparkles: feat: toggle cart and lightbox

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -23,3 +23,38 @@ window.addEventListener('DOMContentLoaded', () => {
   carousel.addEventListener();
   gallery.addEventListener();
 });
+
+(function () {
+  const $portal = document.querySelector('.portal');
+  const $addItemBtn = document.querySelector('.product-info__button');
+
+  $addItemBtn.addEventListener('click', () => {
+    $portal.classList.add('show-lightbox');
+  });
+
+  const $lightbox = document.querySelector('.lightbox');
+  const $confirmBtn = $lightbox.querySelector('.lightbox__confirm');
+  const $continueBtn = $lightbox.querySelector('.lightbox__continue');
+  const $closeBtn = $lightbox.querySelector('.close-button');
+
+  [$confirmBtn, $continueBtn, $closeBtn].forEach((item) => {
+    item.addEventListener('click', () => {
+      $portal.classList.remove('show-lightbox');
+    });
+  });
+
+  const $purchases = document.querySelectorAll('.purchases');
+
+  $purchases.forEach((item) => {
+    item.addEventListener('click', () => {
+      $portal.classList.add('show-cart');
+    });
+  });
+
+  const $cart = document.querySelector('.cart');
+  const $closeCartBtn = $cart.querySelector('.close-button');
+
+  $closeCartBtn.addEventListener('click', () => {
+    $portal.classList.remove('show-cart');
+  });
+})();
