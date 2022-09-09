bc5612297b210db0fd5dde0e1f3ac073177f3d40
Jhonatan David <82552187+jotahdavid@users.noreply.github.com>
2022-09-09 19:25:27 -0300

    :recycle: refactor(generateID): generate random big numbers

--- src/scripts/utils/generateID.js
+++ src/scripts/utils/generateID.js
@@ -1,4 +1,10 @@
-let lastID = 1;
-const generateID = () => String(lastID++);
+function randomNumber(max) {
+  return Math.floor(Math.random() * max) + 1;
+}
+
+function generateID() {
+  const random = randomNumber(10) * new Date().getTime();
+  return String(random).slice(0, 12);
+}
 
 export default generateID;
