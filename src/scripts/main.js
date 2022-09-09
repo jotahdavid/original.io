353ce91654ec53eb8a4b03078e2c9ace3ee6bb8a
Jhonatan David <82552187+jotahdavid@users.noreply.github.com>
2022-09-09 14:40:00 -0300

    :recycle: refactor: change structure scripts folder

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,7 @@
+import Carousel from './components/Carousel/index.js';
+import Gallery from './components/Gallery/index.js';
+
 import { addMatchMediaEvents, smallOnlyMedia } from './matchMedia.js';
-import Carousel from './carousel.js';
-import Gallery from './gallery.js';
 
 const carousel = new Carousel('.carousel-vertical__gallery');
 const gallery = new Gallery('.carousel-horizontal', { perPage: 4, onChangePage: handleChangePage });
