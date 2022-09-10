7620bbfe6da695946f62822e8f2834db2a83f16b
Jhonatan David <82552187+jotahdavid@users.noreply.github.com>
2022-09-10 11:56:26 -0300

    :recycle: refactor(createElement): add JSDoc types

--- src/scripts/utils/createElement.js
+++ src/scripts/utils/createElement.js
@@ -1,13 +1,16 @@
+/**
+ * @param {string | HTMLElement} tag
+ * @param {Record<string, string | number | boolean | Function>} attributes
+ * @param  {...string | Node} content
+ * @returns {HTMLElement}
+ */
 export default function createElement(tag, attributes, ...content) {
-  const $element = !(tag instanceof Element) ? document.createElement(tag) : tag;
+  const $element = !(tag instanceof HTMLElement) ? document.createElement(tag) : tag;
   if (attributes) {
     for (const [attribute, value] of Object.entries(attributes)) {
       const attr = attribute.toLowerCase();
       if (attr === 'classname') {
         $element.setAttribute('class', value);
-      } else if (attr === 'checked') {
-        if ($element.checked === undefined) continue;
-        $element.checked = value;
       } else if (attr.startsWith('on') && typeof value === 'function') {
         $element.addEventListener(attr.slice(2), value);
       } else if (attr === 'html') {
