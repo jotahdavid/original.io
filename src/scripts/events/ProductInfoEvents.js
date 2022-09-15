e8bdbd28c492f06b3c7ed781fb107959bcb871e3
Jhonatan David <82552187+jotahdavid@users.noreply.github.com>
2022-09-15 09:50:57 -0300

    feat(product-info): be able to change the item size

--- src/scripts/events/ProductInfoEvents.js
+++ src/scripts/events/ProductInfoEvents.js
@@ -2,6 +2,7 @@ import capitalize from '../utils/capitalize.js';
 
 const $productInfo = document.querySelector('.product-info');
 const $colorOptions = $productInfo.querySelectorAll('.product-info__color-options__item');
+const $sizeOptions = $productInfo.querySelectorAll('.product-info__size-options__item');
 
 function handleProductColorChange({ currentTarget }) {
   $colorOptions.forEach((item) => {
@@ -12,14 +13,32 @@ function handleProductColorChange({ currentTarget }) {
     item.classList.remove('--active');
   });
 
-  const $colorName = $productInfo.querySelector('.product-info__chosen-option');
+  const $colorName = $productInfo.querySelector('.product-info__color-line .product-info__chosen-option');
   const color = currentTarget.getAttribute('value');
 
   $colorName.textContent = `(${capitalize(color)})`;
 }
 
+function handleProductSizeChange({ currentTarget }) {
+  $sizeOptions.forEach((item) => {
+    if (item === currentTarget) {
+      item.classList.add('--active');
+      return;
+    }
+    item.classList.remove('--active');
+  });
+
+  const $size = $productInfo.querySelector('.product-info__size-line .product-info__chosen-option');
+  const size = currentTarget.getAttribute('value');
+
+  $size.textContent = `(${size})`;
+}
+
 export function addProductInfoEvents() {
   $colorOptions.forEach((item) => {
     item.addEventListener('click', handleProductColorChange);
+  });
+  $sizeOptions.forEach((item) => {
+    item.addEventListener('click', handleProductSizeChange);
   });
 }
